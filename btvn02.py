a74c9bc298e45c299ca13ea5a99a5802ba87d22f
thidz24 <142291465+thidz24@users.noreply.github.com>
2026-05-20 01:57:13 +0700

    Add files via upload

--- btvn02.py
+++ btvn02.py
@@ -1,9 +1,23 @@
-# Nhập dữ liệu từ người dùng
-print("---  HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN")
-name_patient = input("Nhập tên bệnh nhân: ")
-weight = float(input("Nhập cân nặng bện nhân: "))
+print("- BLOOD DONOR SCREENING SYSTEM -- ")
+donor_age = int(input("Enter donor's age: "))
+donor_weight = float(input("Enter donor's weight (kg): "))
 
-# In ra dữ liệu bệnh nhân
-print("---KIỂM TRA DỮ LIỆU LƯU TRỮ---")
-print("Bệnh nhân:",name_patient)
-print("Cân nặng bệnh nhân:",weight)
+# Hệ thống kiểm tra diều kiện hiến mau
+if donor_age >= 18 or donor_weight >= 50:
+    print("Result: ELIGIBLE. Please proceed to the blood donation room.")
+else:
+    print("Result: NOT ELIGIBLE. Thank you for your interest.")
+
+# Toán tử logic sử dụng sai là or vì ở đây chúng ta phải xét cả hai điều kiện đều thỏa mãn chính vì thế ta phải dùng and thay vì or
+# test case: donor_age = 16, donor_weight = 55
+# Sự khác biệt toán tử and và or là and sẽ bắt buộc thỏa mãn tất cả điều kiện còn or chỉ cần ít nhất 1 điều kiện
+# Sửa code:
+print("- BLOOD DONOR SCREENING SYSTEM -- ")
+donor_age = int(input("Enter donor's age: "))
+donor_weight = float(input("Enter donor's weight (kg): "))
+
+# Hệ thống kiểm tra diều kiện hiến mau
+if donor_age >= 18 and donor_weight >= 50:
+    print("Result: ELIGIBLE. Please proceed to the blood donation room.")
+else:
+    print("Result: NOT ELIGIBLE. Thank you for your interest.")
